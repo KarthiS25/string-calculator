55cf72fc15757791fab3861028bd09a836e57b50
KarthiS25 <karthis1015@gmail.com>
2025-04-15 14:02:06 +0530

    Add the default delimiter

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,5 +1,9 @@
 class StringCalculator
   def add(numbers)
-    return 0 if numbers.strip.empty? 
+    return 0 if numbers.strip.empty?
+
+    default_delimiters = /,|\n/ # Default delimiter
+    number_strings = numbers.split(/#{default_delimiters}/).map(&:strip)
+    number_strings.map(&:to_i).sum
   end
 end
