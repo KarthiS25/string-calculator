86ff2a2863256385ebbd81dc505127f090054cc6
KarthiS25 <karthis1015@gmail.com>
2025-04-15 17:04:04 +0530

    Add the 3 test case methods
    - test empty string
    
    - test single number
    
    - test sum of two numbers

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -5,4 +5,16 @@ class StringCalculatorTest < Minitest::Test
   def setup
     @calculator = StringCalculator.new
   end
+
+  def test_returns_0_for_empty_string
+    assert_equal 0, @calculator.add("")
+  end
+
+  def test_returns_number_for_single_number
+    assert_equal 1, @calculator.add("1")
+  end
+
+  def test_returns_sum_of_two_numbers
+    assert_equal 6, @calculator.add("1,5")
+  end
 end
