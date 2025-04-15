97dfc7f51708f2168e917be0a46751e47dfbcead
KarthiS25 <karthis1015@gmail.com>
2025-04-15 17:05:59 +0530

    Add the multiple number and negative number test cases

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -15,6 +15,28 @@ class StringCalculatorTest < Minitest::Test
   end
 
   def test_returns_sum_of_two_numbers
-    assert_equal 6, @calculator.add("1,5")
+    assert_equal 6, @calculator.add("1, 5")
+  end
+
+  def test_returns_sum_of_multiple_numbers
+    assert_equal 10, @calculator.add("1, 2, 3, 4")
+  end
+
+  def test_handles_newline_as_separator
+    assert_equal 6, @calculator.add("1\n2, 3")
+  end
+
+  def test_handles_custom_delimiter
+    assert_equal 3, @calculator.add("//;\n1;2")
+  end
+
+  def test_raises_error_for_negative_number
+    error = assert_raises(RuntimeError) { @calculator.add("1, -2, 3") }
+    assert_equal "negative numbers not allowed -2", error.message
+  end
+
+  def test_raises_error_for_multiple_negative_numbers
+    error = assert_raises(RuntimeError) { @calculator.add("1, -2, -3") }
+    assert_equal "negative numbers not allowed -2,-3", error.message
   end
 end
