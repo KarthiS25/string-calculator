96aef48c06decc17f6657ab7318897978cc7b155
KarthiS25 <karthis1015@gmail.com>
2025-04-15 14:07:32 +0530

    Add the custom delimiter condition

--- string_calculator.rb
+++ string_calculator.rb
@@ -3,6 +3,14 @@ class StringCalculator
     return 0 if numbers.strip.empty?
 
     default_delimiters = /,|\n/ # Default delimiter
+
+    # Custom delimiter
+    if numbers.start_with?("//")
+      delimiter_line, numbers = numbers.split("\n", 2)
+      custom_delimiter = delimiter_line[2..]
+      default_delimiters = Regexp.escape(custom_delimiter)
+    end
+
     number_strings = numbers.split(/#{default_delimiters}/).map(&:strip)
     number_strings.map(&:to_i).sum
   end
